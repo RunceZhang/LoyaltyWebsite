e0509b59ed2d64c7c9f5cfba327e6f0ea0515264
unknown <runce001@gmail.com>
2025-04-08 12:40:46 -0400

    fix router wrapping

--- frontend/loyal-website/src/App.js
+++ frontend/loyal-website/src/App.js
@@ -1,7 +1,7 @@
 // App.js - The main application component
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import ResetPasswordPage from './pages/auth/ResetPasswordPage';
@@ -20,40 +20,36 @@ import './App.css';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/reset-password/:resetToken" element={<ResetPasswordPage />} />
-          
-          {/* Protected routes within Layout */}
-          <Route element={<PrivateRoute><Layout /></PrivateRoute>}>
-            <Route path="/" element={<Dashboard />} />
-            
-            {/* User routes */}
-            <Route path="/profile" element={<UserProfilePage />} />
-            <Route path="/register" element={<PrivateRoute requiredRole="cashier"><RegisterPage /></PrivateRoute>} />
-            
-            {/* Transaction routes */}
-            <Route path="/transactions" element={<TransactionPage />} />
-            <Route path="/transactions/history" element={<TransactionListPage />} />
-            <Route path="/transactions/:transactionId" element={<TransactionDetailPage />} />
-            
-            {/* Event routes */}
-            <Route path="/events" element={<EventListPage />} />
-            <Route path="/events/:eventId" element={<EventDetailPage />} />
-            
-            {/* Promotion routes */}
-            <Route path="/promotions" element={<PromotionListPage />} />
-          </Route>
-          
-          {/* Catch all route */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Routes>
+      {/* Public routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/reset-password/:resetToken" element={<ResetPasswordPage />} />
+      
+      {/* Protected routes within Layout */}
+      <Route element={<PrivateRoute><Layout /></PrivateRoute>}>
+        <Route path="/" element={<Dashboard />} />
+        
+        {/* User routes */}
+        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/register" element={<PrivateRoute requiredRole="cashier"><RegisterPage /></PrivateRoute>} />
+        
+        {/* Transaction routes */}
+        <Route path="/transactions" element={<TransactionPage />} />
+        <Route path="/transactions/history" element={<TransactionListPage />} />
+        <Route path="/transactions/:transactionId" element={<TransactionDetailPage />} />
+        
+        {/* Event routes */}
+        <Route path="/events" element={<EventListPage />} />
+        <Route path="/events/:eventId" element={<EventDetailPage />} />
+        
+        {/* Promotion routes */}
+        <Route path="/promotions" element={<PromotionListPage />} />
+      </Route>
+      
+      {/* Catch all route */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
